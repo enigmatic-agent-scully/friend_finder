cacef488e3640b7b85d1dc5a661ac45679f3d16b
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-11-11 22:25:00 -0500

    on its way!

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var friendData = require('../data/friends');
+
+module.exports = (app) => {
+  app.get('/api/friends', (req, res) => {
+    res.json(friendData);
+  });
+  app.post('/api/friends', (req, res) => {
+    friendData.push(req.body);
+
+  })
+}
+
--- app/server.js
+++ app/server.js
@@ -1,2 +1,16 @@
 var PATH = require('path');
 var express = require('express');
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log("App listening on PORT " + PORT);
+});
