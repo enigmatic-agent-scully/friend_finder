91af17877a01ded9a73e33f0848a4b383a17b78f
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-11-13 18:30:55 -0500

    somewhere

---
+++ app/quiz.js
@@ -0,0 +1,41 @@
+const answers = [
+  {
+    text: 'YES',
+    value: 1
+  },
+  {
+    text: 'yes',
+    value: 2
+  },
+  {
+    text: 'yes and no',
+    value: 3
+  },
+  {
+    text: 'no',
+    value: 4
+  },
+  {
+    text: 'NO',
+    value: 5
+  }
+];
+
+const questions = [
+  {
+    q: 'You often contemplate the complexities of life and the universe.',
+    a: answers
+  },
+  {
+    q: ''
+  }
+];
+
+const quiz = $('#quiz');
+const submit = $('#submit');
+
+const displayQ = () => {
+
+}
+
+
