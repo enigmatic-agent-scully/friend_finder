3b45034bfd2fe636b7cd345957b8a683602e656c
AJ <amanda.elisabet.jordan@gmail.com>
2018-12-19 23:23:33 -0500

    modal works!

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,43 +2,23 @@ var friendData = [
   {
     name: 'Baraka Mahili',
     github_user: 'me@me.com',
-    face: 'www.ggogle.com',
+    face:
+      'http://www.marymarcusfiction.com/wp-content/uploads/2015/09/yay-54383329058-296x300.jpeg',
     scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
   },
   {
-    name: 'John',
+    name: 'AJ',
     github_user: 'me@me.com',
-    face: 'www.ggogle.com',
-    scores: [
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
+    face: 'https://avatars0.githubusercontent.com/u/40344292?s=460&v=4',
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
     name: 'Jane',
     github_user: 'me@me.com',
-    face: 'www.ggogle.com',
-    scores: [
-      2,
-      3,
-      4,
-      2,
-      5,
-      5,
-      4,
-      3,
-      4,
-      1
-    ]
-  },
+    face:
+      'http://www.marymarcusfiction.com/wp-content/uploads/2015/09/yay-54383329058-296x300.jpeg',
+    scores: [2, 3, 4, 2, 5, 5, 4, 3, 4, 1]
+  }
 ];
 
 module.exports = friendData;
